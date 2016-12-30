58fbbd20d0518ec16c208611ab4d1fcfa1ffec2d
buckie <wjmartino@gmail.com>
2016-12-30 15:09:03 -0500

    more work on the first analysis engine
    Signed-off-by: buckie <wjmartino@gmail.com>

--- src/Pact/Analyze/Types.hs
+++ src/Pact/Analyze/Types.hs
@@ -1,14 +1,18 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Pact.Analyze.Types where
 
-import Control.Monad.Trans.Reader
+import Control.Monad.Trans.RWS.Strict
+import Control.Lens
 import Pact.Typecheck
 import Pact.Types
 import Data.Either
 import Data.Decimal
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Text.PrettyPrint.ANSI.Leijen hiding ((<$>),(<$$>))
@@ -19,60 +23,50 @@ data SymType = SymInteger
   | SymBool
   deriving (Show, Eq, Ord)
 
-data TrackingStatus = IsTracked
-  | Untracked
-  | LostTrack String deriving (Show, Eq)
+newtype SymName = SymName { unSymName :: String } deriving (Show, Eq)
+newtype Smt2Cmd = Smt2Cmd { unSmt2Cmd :: String } deriving (Show, Eq)
+
+data TrackingStatus = Tracked
+  | Untracked {_tWhy :: String}
+  | LostTrack {_tWhy :: String}
+  deriving (Show, Eq)
 
 data SymVar = SymVar
-  { _svName :: String
-  , _svType :: SymType
+  { _svName :: SymName
+  , _svType :: Maybe SymType
   , _svTracked :: TrackingStatus
   } deriving (Show, Eq)
+makeLenses ''SymVar
 
 data ProverCtx = ProverCtx
-  { _pcVars :: Map TcId SymVar
-  , _pcPrevSMT :: [String]
-  , _pcNodeSMT :: [String]
+  { _pcInputArgs :: Map TcId SymVar
   } deriving (Show, Eq)
+makeLenses ''ProverCtx
+
+data ProverState = ProverState
+  { _psVars :: Map TcId SymVar
+  , _psPrevSMT :: [Smt2Cmd]
+  , _psNodeSMT :: [Smt2Cmd]
+  } deriving (Show, Eq)
+makeLenses ''ProverState
 
 data SymAst =
   Branch
-    { _brLeft :: SymAst
-    , _brRight :: SymAst
-    , _saProverCtx :: ProverCtx } |
+    { _brRegularLeaf :: SymAst
+    , _brNegatedLeaf :: SymAst
+    , _saProverState :: ProverState } |
   ConstrainedLeaf
-    { _saProverCtx :: ProverCtx } |
+    { _saProverState :: ProverState } |
   ErrorLeaf
     { _elWhy :: String
-    , _saProverCtx :: ProverCtx }
+    , _saProverState :: ProverState }
 
-type PactAnalysis = Reader ProverCtx SymAst
-
-_inferGtTen :: IO (Either (String, Fun TcId) (Fun (TcId, Type)), TcState)
-_inferGtTen = _infer "examples/analyze-tests/analyze-tests.repl" "analyze-tests" "gt-ten"
-
-getGtTenFun :: IO (Fun (TcId, Type))
-getGtTenFun = do
-  (Right f, _) <- _inferGtTen
-  return f
-
-newtype Smt2Cmd = Smt2Cmd { unSmt2Cmd :: String } deriving (Show)
+type PactAnalysis a = RWST ProverCtx () ProverState IO a
 
 data LitVal =
     LVDecimal Decimal
   | LVBool Bool
   | LVInteger Integer
-
--- data Var =
---   Var
---     { _vId :: TcId
---     , _vType :: Type
---     } |
---   Lit
---     { _vId :: TcId
---     , _vType :: Type
---     , _vVal :: LitVal
---     }
 
 pattern NativeFunc f <- (FNative _ f _ _)
 
@@ -81,14 +75,93 @@ pattern Args_Lit lit <- [(Lit _ _ (LVLit (lit)))]
 pattern Args_Var_Var var1 var2 <- [(Var _ var1),(Var _ var2)]
 pattern Args_Var_Lit var lit <- [(Var _ var),(Lit _ _ (LVLit (lit)))]
 pattern Args_Lit_Var lit var <- [(Lit _ _ (LVLit (lit))),(Var _ var)]
+pattern Args_App_Lit_Var app' lit var <- [app',(Lit _ _ (LVLit (lit))),(Var _ var)]
+pattern Args_App_Lit_Lit app' lit1 lit2 <- [app',(Lit _ _ (LVLit (lit1))),(Lit _ _ (LVLit (lit2)))]
 
-pattern GT_Var_Lit_Int name' val' <- (App _ (NativeFunc ">") (Args_Var_Lit (name',_) (LInteger val')))
-pattern LT_Var_Lit_Int name' val' <- (App _ (NativeFunc "<") (Args_Var_Lit (name',_) (LInteger val')))
+pattern NativeFunc_Var_Lit_Int f var' val' <- (App _ (NativeFunc f) (Args_Var_Lit var' (LInteger val')))
+pattern IF_App_Lit_Lit app' lit1 lit2 <- (App _ (NativeFunc "if") (Args_App_Lit_Lit app' lit1 lit2))
 
-pryAppart :: AST (TcId,Type) -> (TcId, Integer)
-pryAppart (GT_Var_Lit_Int n v) = (n,v)
-pryAppart (LT_Var_Lit_Int n v) = (n,v)
+cmpOperators :: Set String
+cmpOperators = Set.fromList [">", "<", ">=", "<=", "="]
 
+analyze :: AST (TcId,Type) -> PactAnalysis SymAst
+analyze (NativeFunc_Var_Lit_Int f v@(name', _) l)
+-- Comparison Operators
+  | Set.member f cmpOperators = do
+      updatePrevCommands
+      updateTrackedVars v
+      state' <- get
+      sv <- getSymVar name'
+      if isTracked sv
+      then do
+        (regular', negated') <- return $! createCmpAssertions f (unSymName (view svName sv)) (show l)
+        return $ Branch
+          { _brRegularLeaf = ConstrainedLeaf (state' { _psNodeSMT = [regular'] })
+          , _brNegatedLeaf = ConstrainedLeaf (state' { _psNodeSMT = [negated'] })
+          , _saProverState = state' }
+      else return $ ErrorLeaf
+          { _elWhy = "Variable " ++ show (view svName sv) ++ " is not tracked"
+          , _saProverState = state' }
+
+createCmpAssertions :: String -> String -> String -> (Smt2Cmd, Smt2Cmd)
+createCmpAssertions f a b = (regular', negated')
+  where
+    regular' = Smt2Cmd $ "(assert (> " ++ a ++ " " ++ b ++ "))"
+    negated' = Smt2Cmd $ "(assert (not (> " ++ a ++ " " ++ b ++ ")))"
+
+updateTrackedVars :: (TcId,Type) -> PactAnalysis ()
+updateTrackedVars (name', type') = do
+  isInputArg <- Map.member name' <$> view pcInputArgs
+  alreadyTracked <- Map.member name' <$> use psVars
+  if isInputArg || alreadyTracked
+    then return ()
+    else do
+      psVars %= Map.insert name' (constructSymVar (name', type'))
+
+updatePrevCommands :: PactAnalysis ()
+updatePrevCommands = do
+  lastCmds <- use psNodeSMT
+  psPrevSMT %= (\prevCmds -> prevCmds ++ lastCmds)
+  psNodeSMT .= []
+
+convertName :: TcId -> SymName
+convertName = SymName . show
+
+convertType :: Type -> Maybe SymType
+convertType TyInteger = Just SymInteger
+convertType TyBool = Just SymBool
+convertType TyDecimal = Just SymDecimal
+convertType _ = Nothing
+
+constructSymVar :: (TcId, Type) -> SymVar
+constructSymVar (name', type') = newVar
+  where
+    convType = convertType type'
+    convName = convertName name'
+    trackingStatus = case convType of
+      Nothing -> Untracked "Unsupported Type"
+      Just _ -> Tracked
+    newVar = SymVar { _svName = convName, _svType = convType, _svTracked = trackingStatus}
+
+getSymVar :: TcId -> PactAnalysis SymVar
+getSymVar name' = do
+  sv1 <- Map.lookup name' <$> view pcInputArgs
+  case sv1 of
+    Just sv' -> return sv'
+    Nothing -> do
+      sv2 <- Map.lookup name' <$> use psVars
+      case sv2 of
+        Just sv' -> return sv'
+        Nothing -> do
+          reader' <- ask
+          state' <- get
+          error $ "unable to lookup variable: " ++ show name'
+            ++ "\n### context ###\n" ++ show reader'
+            ++ "\n### state ###\n" ++ show state'
+
+isTracked :: SymVar -> Bool
+isTracked (SymVar _ _ Tracked) = True
+isTracked _ = False
 -- (module analyze-tests 'analyze-admin-keyset
 --   (defun gt-ten (a:integer)
 --     (if (> a 10) "more than ten" "less than ten")
@@ -123,3 +196,11 @@ pryAppart (LT_Var_Lit_Int n v) = (n,v)
 --              , Lit {_aId = string5
 --                    , _aLitType = string
 --                    , _aLitValue = LVLit "less than ten"}]}]}
+
+_inferGtTen :: IO (Either (String, Fun TcId) (Fun (TcId, Type)), TcState)
+_inferGtTen = _infer "examples/analyze-tests/analyze-tests.repl" "analyze-tests" "gt-ten"
+
+getGtTenFun :: IO (Fun (TcId, Type))
+getGtTenFun = do
+  (Right f, _) <- _inferGtTen
+  return f
