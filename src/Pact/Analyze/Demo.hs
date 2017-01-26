75656fadd19f1be61eed1474c48f838a3944e84e
Stuart Popejoy <spopejoy@panix.com>
2017-01-26 10:02:01 -0800

    ux stuff

--- src/Pact/Analyze/Demo.hs
+++ src/Pact/Analyze/Demo.hs
@@ -2,6 +2,12 @@
 module Pact.Analyze.Demo where
 
 import Pact.Analyze.DSL
+import System.Environment
 
 runDemo :: IO ()
-runDemo = _compileTests False "examples/analyze-tests/analyze-tests.repl" "analyze-tests" "pay"
+runDemo = do
+  as <- getArgs
+  let (fp,m,f) = case as of
+        [a,b,c] -> (a,b,c)
+        _ -> ( "examples/analyze-tests/analyze-tests.repl","analyze-tests","pay")
+  _compileTests False fp m f
