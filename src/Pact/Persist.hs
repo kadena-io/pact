fb2f4cb9a767f0c0e37d12209b3219cafd0cf263
Stuart Popejoy <spopejoy@panix.com>
2017-02-27 14:28:41 -0500

    KeyQuery DSL, SQLite working

--- src/Pact/Persist.hs
+++ src/Pact/Persist.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Pact.Persist
   (Persist,
    Table(..),
    KeyCmp(..),
-   cmpToOp,
-   KeyQuery(..),
+   cmpToOp,conjToOp,
+   KeyQuery(..),kAnd,kOr,compileQuery,
    Persister(..),
    WriteType(..),throwDbError,
    Text
@@ -16,6 +17,7 @@ module Pact.Persist
 import Data.Aeson
 import Data.Text (Text)
 import Data.String
+import Data.Monoid
 
 import Pact.Types.Runtime (WriteType(..),throwDbError)
 
@@ -29,7 +31,12 @@ deriving instance Show (Table k)
 deriving instance Eq (Table k)
 deriving instance Ord (Table k)
 
-data KeyCmp = KGT|KGTE|KEQ|KLT|KLTE deriving (Eq,Show,Ord,Enum)
+data KeyCmp = KGT|KGTE|KEQ|KNEQ|KLT|KLTE deriving (Eq,Show,Ord,Enum)
+data KeyConj = AND|OR deriving (Eq,Show,Ord,Enum)
+
+-- key query stuff
+-- a key value pairs with an operator: >= k, < k, etc.
+-- connectors join operators: (>= k1 -AND- < k2) -OR- /= k3
 
 -- | SQL equivalents for 'KeyCmp'
 cmpToOp :: IsString s => KeyCmp -> s
@@ -37,12 +44,39 @@ cmpToOp kc = fromString $ case kc of
   KGT -> ">"
   KGTE -> ">="
   KEQ -> "="
+  KNEQ -> "!="
   KLT -> "<"
   KLTE -> "<="
 
+-- | SQL equivalents for 'KeyConj'
+conjToOp :: IsString s => KeyConj -> s
+conjToOp c = fromString $ case c of
+  AND -> "AND"
+  OR -> "OR"
+
 data KeyQuery k =
-  KQAll |
-  KQKey { _kqKey :: k, _kqCmp :: KeyCmp }
+  KQKey { kqCmp :: KeyCmp, kqKey :: k } |
+  KQConj { kqL :: KeyQuery k, kqConj :: KeyConj, kqR :: KeyQuery k }
+  deriving (Eq,Show)
+
+-- | Convenience for infix usage.
+kAnd :: KeyQuery k -> KeyQuery k -> KeyQuery k
+kAnd l r = KQConj l AND r
+-- | Convenience for infix usage.
+kOr :: KeyQuery k -> KeyQuery k -> KeyQuery k
+kOr l r = KQConj l OR r
+
+-- | Compile an optional 'KeyQuery' to standard SQL statement syntax with param values list.
+compileQuery :: (IsString s, Monoid s) => s -> Maybe (KeyQuery k) -> (s,[k])
+compileQuery _ Nothing = ("",[])
+compileQuery keyfield (Just kq) = ("WHERE " <> qs,pms)
+  where (qs,pms) = compile True kq
+        compile _ (KQKey q k) = (keyfield <> " " <> cmpToOp q <> " ?",[k])
+        compile top (KQConj l o r) = (op <> lq <> " " <> conjToOp o <> " " <> rq <> cp,lps ++ rps)
+          where (op,cp) | top = ("","")
+                        | otherwise = ("(",")")
+                (lq,lps) = compile False l
+                (rq,rps) = compile False r
 
 data Persister s = Persister {
   createTable :: forall k . Table k -> Persist s ()
@@ -53,9 +87,14 @@ data Persister s = Persister {
   ,
   rollbackTx :: Persist s ()
   ,
-  getKeys :: forall k . Table k -> KeyQuery k -> Persist s [k]
+  queryKeys :: forall k . Table k -> Maybe (KeyQuery k) -> Persist s [k]
   ,
-  readData :: forall k v . FromJSON v => Table k -> k -> Persist s (Maybe v)
+  query :: forall k v . FromJSON v => Table k -> Maybe (KeyQuery k) -> Persist s [(k,v)]
   ,
-  writeData :: forall k v . ToJSON v => Table k -> WriteType -> k -> v -> Persist s ()
+  readValue :: forall k v . FromJSON v => Table k -> k -> Persist s (Maybe v)
+  ,
+  writeValue :: forall k v . ToJSON v => Table k -> WriteType -> k -> v -> Persist s ()
   }
+
+_compileQry1 :: (String,[Int])
+_compileQry1 = compileQuery "key" (Just (KQKey KGT 2 `kAnd` KQKey KLT 4 `kOr` KQKey KNEQ 10))
--- src/Pact/Persist/SQLite.hs
+++ src/Pact/Persist/SQLite.hs
@@ -6,9 +6,10 @@ module Pact.Persist.SQLite
   (SQLite(..),initSQLite,persister)
   where
 
+import Control.Arrow
 import Control.Monad
 import Data.Aeson
-import Data.Map.Strict as M
+import qualified Data.Map.Strict as M
 import Data.Monoid
 import qualified Data.Text as T
 import Data.Text.Encoding
@@ -16,6 +17,8 @@ import Data.String
 import Database.SQLite3.Direct as SQ3
 import Prelude hiding (log)
 import qualified Data.ByteString.Lazy as BSL
+import System.Directory
+import Control.Monad.State
 
 import Pact.Persist
 import Pact.Types.SQLite
@@ -62,11 +65,13 @@ persister = Persister {
   ,
   rollbackTx = \s -> execs_ (tRollback (txStmts s)) >> return (s,())
   ,
-  getKeys = \t kq s -> (s,) <$> getKeys' t kq s
+  queryKeys = \t kq s -> (s,) <$> queryKeys' t kq s
   ,
-  readData = \t k s -> (s,) <$> readData' t k s
+  query = \t kq s -> (s,) <$> query' t kq s
   ,
-  writeData = \t wt k v s -> (s,) <$> writeData' t wt k v s
+  readValue = \t k s -> (s,) <$> readData' t k s
+  ,
+  writeValue = \t wt k v s -> (s,) <$> writeData' t wt k v s
 
 
   }
@@ -98,6 +103,10 @@ expectSing :: Show a => String -> [a] -> IO a
 expectSing _ [s] = return s
 expectSing desc v = throwDbError $ "Expected single-" ++ desc ++ " result, got: " ++ show v
 
+expectTwo :: Show a => String -> [a] -> IO (a,a)
+expectTwo _ [a,b] = return (a,b)
+expectTwo desc v = throwDbError $ "Expected two-" ++ desc ++ " result, got: " ++ show v
+
 kTextTys :: KeyTys Text
 kTextTys = KeyTys "text" (SText . toUtf8) RText decodeText
 kIntTys :: KeyTys Int
@@ -122,14 +131,25 @@ createTable' t e = do
            mkstmt ("SELECT VALUE FROM " <> tn <> " WHERE KEY = ?")
   return $ e { tableStmts = M.insert tn ss (tableStmts e) }
 
-getKeys' :: Table k -> KeyQuery k -> SQLite -> IO [k]
-getKeys' t kq e = do
-  let (whereQ,inParams) = case kq of
-        KQAll -> ("",[])
-        KQKey k q -> (" WHERE key " <> cmpToOp q <> " ? ",[inFun (kTys t) k])
-  rs <- qry (conn e) ("SELECT key FROM " <> tableName t <> whereQ <> " ORDER BY key")
-        inParams [outTy (kTys t)]
+
+query' :: FromJSON v => Table k -> Maybe (KeyQuery k) -> SQLite -> IO [(k,v)]
+query' t kq e = do
+  let (cols,outParams) = ("key, value",[outTy (kTys t),RText])
+  rs <- doQuery t kq cols outParams e
+  mapM (expectTwo "field" >=> (\(k,v) -> (,) <$> outFun (kTys t) k <*> decodeBlob v)) rs
+
+queryKeys' :: Table k -> Maybe (KeyQuery k) -> SQLite -> IO [k]
+queryKeys' t kq e = do
+  let (cols,outParams) = ("key",[outTy (kTys t)])
+  rs <- doQuery t kq cols outParams e
   mapM (expectSing "field" >=> outFun (kTys t)) rs
+
+doQuery :: Table k -> Maybe (KeyQuery k) -> Utf8 -> [RType] -> SQLite -> IO [[SType]]
+doQuery t kq cols outParams e = do
+  let (whereQ,inParams) = second (map (inFun (kTys t))) $ compileQuery "key" kq
+      sql = "SELECT " <> cols <> " FROM " <> tableName t <> " " <> whereQ <> " ORDER BY key"
+  -- log e (show sql)
+  qry (conn e) sql inParams outParams
 
 getStmts :: SQLite -> Table k -> IO TableStmts
 getStmts e t = case M.lookup (tableName t) (tableStmts e) of
@@ -166,3 +186,40 @@ initSQLite ps logFn f = do
         }
   runPragmas (conn s) ps
   return s
+
+
+_test :: IO ()
+_test = do
+  let db = "log/test.sqlite"
+  doesFileExist db >>= \b -> when b (removeFile db)
+  e <- initSQLite [] putStrLn db
+  let p = persister
+      dt = DataTable "data"
+      tt = TxTable "tx"
+      run f = do
+        s <- get
+        (s',r) <- liftIO (f s)
+        put s'
+        return r
+  (`evalStateT` e) $ do
+    run $ beginTx p
+    run $ createTable p dt
+    run $ createTable p tt
+    run $ commitTx p
+    run $ beginTx p
+    run $ writeValue p dt Insert "stuff" (String "hello")
+    run $ writeValue p dt Insert "tough" (String "goodbye")
+    run $ writeValue p tt Write 1 (String "txy goodness")
+    run $ writeValue p tt Insert 2 (String "txalicious")
+    run $ commitTx p
+    run (readValue p dt "stuff") >>= (liftIO . (print :: Maybe Value -> IO ()))
+    run (query p dt (Just (KQKey KEQ "stuff"))) >>=
+      (liftIO . (print :: [(Text,Value)] -> IO ()))
+    run (queryKeys p dt (Just (KQKey KGTE "stuff"))) >>= liftIO . print
+    run (query p tt (Just (KQKey KGT 0 `kAnd` KQKey KLT 2))) >>=
+      (liftIO . (print :: [(Int,Value)] -> IO ()))
+    run $ beginTx p
+    run $ writeValue p tt Update 2 (String "txalicious-2!")
+    run (readValue p tt 2) >>= (liftIO . (print :: Maybe Value -> IO ()))
+    run $ rollbackTx p
+    run (readValue p tt 2) >>= (liftIO . (print :: Maybe Value -> IO ()))
