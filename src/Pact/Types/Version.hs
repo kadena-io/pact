121584f82ff2f3e596e0571def4b76e20b312e43
Stuart Popejoy <spopejoy@panix.com>
2017-05-26 14:17:25 -0400

    use VERSION_pact exported by cabal

--- src/Pact/Types/Version.hs
+++ src/Pact/Types/Version.hs
@@ -6,11 +6,11 @@
 -- License    : BSD-style (see the file LICENSE)
 -- Maintainer : Stuart Popejoy <stuart@kadena.io>
 --
--- Uses CPP to access PACT_VERSION from cabal.
+-- Use CPP to retrieve cabal library version.
 --
 module Pact.Types.Version (pactVersion) where
 
 import Data.Text (Text)
 
 pactVersion :: Text
-pactVersion = PACT_VERSION
+pactVersion = VERSION_pact
