beb348aa79a8c397f38f40865c5996918b7ba069
Joel Burget <joelburget@gmail.com>
2018-06-29 11:47:19 -0700

    Move (...) to Pact.Analyze.Util.

--- src/Pact/Analyze/Util.hs
+++ src/Pact/Analyze/Util.hs
@@ -11,6 +11,10 @@ module Pact.Analyze.Util where
 (<&&>) :: (Functor f, Functor g) => f (g a) -> (a -> b) -> f (g b)
 (<&&>) = flip (<$$>)
 
+-- | Function composition that consumes two args instead of one
+(...) :: (a -> b) -> (x -> y -> a) -> x -> y -> b
+(...) = (<$$>)
+
 for2
   :: (Traversable s, Traversable t, Applicative f)
   => s (t a) -> (a -> f b) -> f (s (t b))
