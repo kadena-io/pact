627ceefa1f50817779fe3cacc4be7e54a80b7c74
Brian Schroeder <bts@gmail.com>
2018-08-31 11:07:06 -0400

    Extract translateLet
    This is in anticipation of tracking scopes. The previous method of let
    translation would not permit us to create a single scope for a let form
    -- we would be forced to create one per binding.

--- src/Pact/Analyze/Patterns.hs
+++ src/Pact/Analyze/Patterns.hs
@@ -35,30 +35,12 @@ pattern NativeFunc f <- FNative _ f _ _
 
 -- compileNode's Patterns
 
--- NOTE: For now, we don't handle an inlined App's synthetic (CBV) let bindings
---       specially -- we'll let AST_Let handle these bindings until a refactor
---       that will soon land (from the `trace-scopes` branch). At that point,
---       we can get rid of 'letBindingType', the current version of
---       'AST_InlinedApp', 'mkLet', and change 'AST_Let' to only match
---       'AstBindLet' 'Binding's.
-
--- pattern AST_InlinedApp :: [(Named a, AST a)] -> [AST a] -> AST a
--- pattern AST_InlinedApp bindings body <-
---   App _ (FDefun _ _ _ _ [Binding _ bindings body AstBindInlinedCallArgs]) _args
-
-pattern AST_InlinedApp :: [AST a] -> AST a
-pattern AST_InlinedApp body <- App _ (FDefun _ _ _ _ body) _args
-
-letBindingType :: AstBindType n -> Bool
-letBindingType AstBindLet = True
-letBindingType AstBindInlinedCallArgs = True
-letBindingType (AstBindSchema _) = False
-
-mkLet :: a -> [(Named a, AST a)] -> [AST a] -> AST a
-mkLet node bindings body = Binding node bindings body AstBindLet
+pattern AST_InlinedApp :: [(Named a, AST a)] -> [AST a] -> AST a
+pattern AST_InlinedApp bindings body <-
+  App _ (FDefun _ _ _ _ [Binding _ bindings body AstBindInlinedCallArgs]) _args
 
 pattern AST_Let :: forall a. a -> [(Named a, AST a)] -> [AST a] -> AST a
-pattern AST_Let node bindings body <- Binding node bindings body (letBindingType -> True)
+pattern AST_Let node bindings body = Binding node bindings body AstBindLet
 
 pattern AST_BindSchema :: forall a. a -> [(Named a, AST a)] -> a -> [AST a] -> AST a
 pattern AST_BindSchema node bindings schema body <- Binding node bindings body (AstBindSchema schema)
