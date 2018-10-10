34714e394599c6e7a38018d64f05633df0228ecc
Emily Pillmore <emily@kadena.io>
2018-10-10 12:01:53 -0400

    make flush with Joel/Bryan's work

--- src/Pact/Analyze/Types/Languages.hs
+++ src/Pact/Analyze/Types/Languages.hs
@@ -551,7 +551,7 @@ instance UserShow a => UserShow (Term a) where
     Hash x               -> parenList ["hash", userShow x]
     ReadKeySet name      -> parenList ["read-keyset", userShow name]
     ReadDecimal name     -> parenList ["read-decimal", userShow name]
-    ReadInteger i        -> parenList ["read-integer", userShow i]
+    ReadInteger name     -> parenList ["read-integer", userShow name]
 
 deriving instance Eq a   => Eq (Term a)
 deriving instance Eq a   => Eq (Core Term a)
