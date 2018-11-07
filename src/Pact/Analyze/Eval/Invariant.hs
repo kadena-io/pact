4aa3b4f154d27c90a8817c7051a18b30f69374bb
Brian Schroeder <bts@gmail.com>
2018-11-07 12:10:09 -0500

    Add Query and InvariantCheck Mergeable instances

--- src/Pact/Analyze/Eval/Invariant.hs
+++ src/Pact/Analyze/Eval/Invariant.hs
@@ -5,9 +5,11 @@ module Pact.Analyze.Eval.Invariant where
 import           Control.Lens               (at, view, (%=))
 import           Control.Monad.Except       (MonadError (throwError))
 import           Control.Monad.Reader       (MonadReader, ReaderT)
-import           Control.Monad.State.Strict (MonadState, StateT)
+import           Control.Monad.State.Strict (MonadState,
+                                             StateT(StateT, runStateT))
 import           Data.Map.Strict            (Map)
-import           Data.SBV                   (bnot, (&&&))
+import           Data.SBV                   (Mergeable(symbolicMerge),
+                                             bnot, (&&&))
 
 import           Pact.Analyze.Errors
 import           Pact.Analyze.Eval.Core
@@ -22,6 +24,14 @@ newtype InvariantCheck a = InvariantCheck
     (Either AnalyzeFailure)) a
   } deriving (Functor, Applicative, Monad, MonadError AnalyzeFailure,
     MonadReader (Located (Map VarId AVal)), MonadState SymbolicSuccess)
+
+instance (Mergeable a) => Mergeable (InvariantCheck a) where
+  symbolicMerge force test left right = InvariantCheck $ StateT $ \s0 -> do
+    (resL, sL) <- runStateT (unInvariantCheck left) s0
+    (resR, sR) <- runStateT (unInvariantCheck right) s0
+    pure ( symbolicMerge force test resL resR
+         , symbolicMerge force test sL   sR
+         )
 
 instance Analyzer InvariantCheck where
   type TermOf InvariantCheck = Invariant
