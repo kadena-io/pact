13eeda80dece447b3d941327ebe540f235af7793
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-12-07 15:57:59 -0500

    Perform Linux build with GitLab runner (#329)

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ rpRef ? "f3ff81d519b226752c83eefd4df6718539c3efdc", rpSha ?  "1ijxfwl36b9b2j4p9j3bv8vf7qfi570m1c5fjyvyac0gy0vi5g8j" }:
+{ rpRef ? "f3ff81d519b226752c83eefd4df6718539c3efdc", rpSha ?  "1ijxfwl36b9b2j4p9j3bv8vf7qfi570m1c5fjyvyac0gy0vi5g8j", system ? builtins.currentSystem }:
 
 let rp = builtins.fetchTarball {
   url = "https://github.com/reflex-frp/reflex-platform/archive/${rpRef}.tar.gz";
@@ -6,7 +6,7 @@ let rp = builtins.fetchTarball {
 };
 
 in
-  (import rp {}).project ({ pkgs, ... }: {
+  (import rp { inherit system; }).project ({ pkgs, ... }: {
     name = "pact";
     overrides = self: super:
       let guardGhcjs = p: if self.ghc.isGhcjs or false then null else p;
