fc90ea7560e68bba3d75949e0b747a0225beb0dd
Vaibhav Sagar <vaibhav.sagar@obsidian.systems>
2018-12-10 17:21:50 -0500

    static.nix: make system configurable

--- static.nix
+++ static.nix
@@ -1,8 +1,10 @@
+{ system ? builtins.currentSystem }:
+
 let
   rp = import (builtins.fetchTarball {
     url = "https://github.com/vaibhavsagar/reflex-platform/archive/ae542c3e7ed4fb1b4552f447b1205982e261cd68.tar.gz";
     sha256 = "0p14b4kdjkykkcql8xdp2x8qvw7cla8imikl940a8qcsc49vkwpf";
-  }) {};
+  }) { inherit system; };
   pact-src = builtins.filterSource
     (path: type: !(builtins.elem (baseNameOf path)
         ["result" "dist" "dist-ghcjs" ".git" ".stack-work"]))
