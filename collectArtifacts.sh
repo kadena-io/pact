4909fb647028283f464f1ced84697839242b4ca1
Doug Beardsley <mightybyte@gmail.com>
2018-12-13 18:25:28 -0500

    Artifacts for docs and coverage

---
+++ collectArtifacts.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# Creates artifacts to be captured by GitLab CI
+
+if [ ! -d result ] ; then nix-build ; fi
+if [ ! -d result-doc ] ; then nix-build -A ghc.pact.doc ; fi
+
+rm -fr public
+mkdir -p public
+cp -LR result/ghc/pact/share/hpc/vanilla/html public/code-coverage
+mkdir -p public/docs
+cp -LR `find result-doc/share -name html`/* public/docs
+chmod -R u+w public
