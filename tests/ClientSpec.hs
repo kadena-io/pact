450480bce4cb16224afb1ad07a2cd6f732ff0918
Vaibhav Sagar <vaibhav.sagar@obsidian.systems>
2019-01-25 15:52:35 -0500

    Remove redundant verify endpoint test

--- tests/ClientSpec.hs
+++ tests/ClientSpec.hs
@@ -5,23 +5,18 @@
 module ClientSpec (spec) where
 
 import Data.Aeson
-import Data.Aeson.Types
 import Data.Default (def)
-import Data.Maybe
 import Test.Hspec
-import NeatInterpolation (text)
 import Utils.TestRunner
 import qualified Network.HTTP.Client as HTTP
 import qualified Control.Exception as Exception
 
-import qualified Pact.Analyze.Remote.Types as Analyze
 import Pact.ApiReq
 import Pact.Types.API
 import Pact.Types.Command
 import Data.Text (Text)
 import Pact.Server.Client
 import Servant.Client
-import qualified Data.HashMap.Strict as HM
 
 _testLogDir, _testConfigFilePath, _testPort, _serverPath :: String
 _testLogDir = testDir ++ "test-log/"
@@ -37,30 +32,6 @@ simpleServerCmd :: IO (Command Text)
 simpleServerCmd = do
   simpleKeys <- genKeys
   mkExec  "(+ 1 2)" Null def [simpleKeys] (Just "test1")
-
-singleModule :: Analyze.Request
-singleModule = fromJust $ parseMaybe parseJSON $ (Object (HM.fromList
-  [ ("verify",(Object (HM.fromList [("namespace", Null), ("name", String "helloWorld")])))
-  , ("modules",Array [Object (HM.fromList
-    [("hash",String "5a05b07162915175823e44ec80e134966e12233e0ec0ce1f156241e2f64ed828be0a7c2e267784f4846046e84df80fb8e64f8530b5a6eed4952fe9e9cd4b61a1")
-    ,("blessed",Array [])
-    ,("keyset",String "admin-keyset")
-    ,("interfaces",Array [])
-    ,("name",(Object (HM.fromList [("namespace", Null), ("name", String "helloWorld")])))
-    ,("code",String [text|
-    (module helloWorld 'admin-keyset
-      "A smart contract to greet the world."
-      (defun hello (name)
-        "Do the hello-world dance"
-        (format "Hello {}!" [name]))
-    )
-    |])
-    ,("meta",Object (HM.fromList
-      [ ("model",Array [])
-      , ("docs",String "A smart contract to greet the world.")
-      ]))
-    ])
-  ])]))
 
 spec :: Spec
 spec = around_ bracket $ describe "Servant API client tests" $ do
@@ -85,8 +56,3 @@ spec = around_ bracket $ describe "Servant API client tests" $ do
     res <- runClientM (local cmd) clientEnv
     let cmdData = (CommandSuccess . Number) 3
     res `shouldBe` (Right (ApiSuccess cmdData))
-  it "correctly verifies a property" $ do
-    res <- runClientM (verify singleModule) clientEnv
-    let expt = fromJust $ parseMaybe parseJSON (Object (HM.singleton "output"
-            (Array ["<interactive>:3:16:Warning: Unable to resolve type\n"])))
-    res `shouldBe` (Right expt)
