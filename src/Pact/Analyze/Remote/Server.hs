bc249a67be6bbb0d7a05d4313bd66a7e643f972e
Stuart Popejoy <slpopejoy@users.noreply.github.com>
2019-09-05 14:03:36 -0400

    tests passing

--- src/Pact/Analyze/Remote/Server.hs
+++ src/Pact/Analyze/Remote/Server.hs
@@ -96,7 +96,7 @@ moduleNotFoundP = MP.string "<interactive>:"
                *> digitsP *> MP.char ':'
                *> digitsP *> MP.char ':'
                *> MP.string " Module "
-               *> fmap fromString (MP.some $ MP.notChar ' ')
+               *> fmap fromString (MP.some $ MP.anySingleBut ' ')
                <* MP.string " not found"
   where
     digitsP :: MP.Parsec Void String ()
