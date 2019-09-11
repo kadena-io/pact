4aaf46ff50aca1ce6606b359cc5235e37b45e673
Tom Smalley <tfsmalley@gmail.com>
2019-09-11 19:12:28 +0100

    Remove now unnecessary overrides

--- overrides.nix
+++ overrides.nix
@@ -45,12 +45,6 @@ in {
     sha256 = "1wizfz8vdplz3sf81vh33sny6p8ynhlpvjxqjpsym7ssb186h0f1";
   });
 
-  neat-interpolation = dontCheck (callHackageDirect {
-    pkg = "neat-interpolation";
-    ver = "0.3.2.4";
-    sha256 = "0gygd2f0wbqa668dz7k8jfryilmbyzravlz7ysp3d13n8h00irba";
-  });
-
   sbv = dontCheck (callHackageDirect {
     pkg = "sbv";
     ver = "8.2";
@@ -64,11 +58,5 @@ in {
     rev = "6ee9fcb026ebdb49b810802a981d166680d867c9";
     sha256 = "09fcf896bs6i71qhj5w6qbwllkv3gywnn5wfsdrcm0w1y6h8i88f";
   }) {});
-
-  versions = dontCheck (doJailbreak (callHackageDirect {
-    pkg = "versions";
-    ver = "3.4.0.1";
-    sha256 = "1qr6kgkmd065a4wcl653bz65raivkb47i7k6jczc05nciccb7i2l";
-  }));
 
 }
