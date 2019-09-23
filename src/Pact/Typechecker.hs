8facdbc30a391fe0094558812bd4af198337bfe6
Emily Pillmore <emily@kadena.io>
2019-09-23 15:27:00 -0400

    add bad yield + rollback repl file

--- src/Pact/Typechecker.hs
+++ src/Pact/Typechecker.hs
@@ -802,7 +802,7 @@ assocStepYieldReturns (TopFun (FDefun _ _ _ Defpact _ _ _) _) steps =
         -- check that a cross-chain yield and rollback do not occur
         -- in the same step, otherwise build the tuple
 
-        (Just y, Just{}) ->
+        (Just y, Just{}) -> do
           if _yrCrossChain y
           then die'' step "Illegal rollback with yield"
           else return (n, yr)
