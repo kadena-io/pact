3b203840de73a33f46a87f7f81eeef86a0d5c97f
Stuart Popejoy <slpopejoy@users.noreply.github.com>
2019-10-18 19:41:47 -0400

    Fix defaultInterpreterState, Decimal pretty (#704)
    * Fix defaultInterpreterState, Decimal pretty
    
    * fix test build
    
    * fix SigCapability JSON

--- tests/ParserSpec.hs
+++ tests/ParserSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module ParserSpec (spec) where
 
 import Test.Hspec
@@ -6,11 +8,14 @@ import Pact.Repl.Types
 import Data.Either
 import Control.Monad.State.Strict
 import Pact.Types.Term
+import Pact.Types.Exp
+import Pact.Types.Pretty
 
 spec :: Spec
 spec = do
-  loadBadParens
-  runBadTests
+  describe "loadBadParens" $ loadBadParens
+  describe "runBadTests" $ runBadTests
+  describe "prettyLit" $ prettyLit
 
 evalString' :: String -> IO (Either String (Term Name), ReplState)
 evalString' cmd = initReplState StringEval Nothing >>= runStateT (evalRepl' cmd)
@@ -33,3 +38,16 @@ runBadTests = do
 
   (r',_s) <- runIO $ evalString' "2.5000000000000082438366423843257667709673523588188657691908576057841129660246632518344756443840469044035563870195392340918714818321610099200359876739795884554030084775850222523998548261336376757270608452858970446458373412193311600907493698300484416227447910"
   it "should not parse overly-long decimal literals" $ r' `shouldSatisfy` isLeft
+
+prettyLit :: Spec
+prettyLit = do
+
+  it "123 prints properly" $ pretty (LInteger 123) `shouldBe` "123"
+  it "-123 prints properly" $ pretty (LInteger (-123)) `shouldBe` "-123"
+  it "123.0 prints properly" $ pretty (LDecimal 123.0) `shouldBe` "123.0"
+  it "-123.0 prints properly" $ pretty (LDecimal (- 123.0)) `shouldBe` "-123.0"
+  it "123.45 prints properly" $ pretty (LDecimal 123.45) `shouldBe` "123.45"
+  it "-123.45 prints properly" $ pretty (LDecimal (- 123.45)) `shouldBe` "-123.45"
+  it "true prints properly" $ pretty (LBool True) `shouldBe` "true"
+  it "false prints properly" $ pretty (LBool False) `shouldBe` "false"
+  it "string prints properly" $ pretty (LString "hello world") `shouldBe` "\"hello world\""
