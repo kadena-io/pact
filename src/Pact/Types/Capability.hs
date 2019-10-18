81fc32d9366d83aeca6c23f92ad36fb5ef86287d
Stuart Popejoy <slpopejoy@users.noreply.github.com>
2019-10-18 18:54:19 -0400

    fix SigCapability JSON

--- src/Pact/Types/Capability.hs
+++ src/Pact/Types/Capability.hs
@@ -22,24 +22,19 @@ module Pact.Types.Capability
   , CapabilityType(..), capType
   , CapAcquireResult(..)
   , SigCapability(..)
-  , parseSigCapability
   , Capabilities(..), capStack, capManaged, capManagedSeen
   , CapScope(..)
   , CapSlot(..), csCap, csComposed, csScope
   ) where
 
 import Control.DeepSeq (NFData)
-import Control.Error (fmapL)
 import Control.Lens hiding ((.=),DefName)
 import Data.Aeson
 import Data.Default
 import Data.Set (Set)
-import Data.Text (Text, unpack)
 
 import GHC.Generics
 
-import Pact.Compile
-import Pact.Parse (parsePact)
 import Pact.Types.Lang
 import Pact.Types.Orphans ()
 import Pact.Types.PactValue
@@ -75,24 +70,16 @@ instance NFData SigCapability
 instance Pretty SigCapability where
   pretty SigCapability{..} = parens $ hsep (pretty _scName:map pretty _scArgs)
 
-instance ToJSON SigCapability
-  where toJSON = toJSON . renderCompactText
+instance ToJSON SigCapability where
+  toJSON (SigCapability n args) = object $
+    [ "name" .=  n
+    , "args" .= args
+    ]
 
 instance FromJSON SigCapability where
-  parseJSON = withText "SigCapability" $ \t -> case parseSigCapability t of
-    Right c -> return c
-    Left e -> fail e
-
-parseSigCapability :: Text -> Either String SigCapability
-parseSigCapability txt = parsed >>= compiled >>= parseApp
-  where
-    parseApp ts = case ts of
-      [(TApp (App (TVar (QName q) _) as _) _)] -> SigCapability q <$> mapM toPV as
-      _ -> Left $ "Sig capability parse failed: Expected single qualified capability in form (qual.DEFCAP arg arg ...)"
-    compiled ParsedCode{..} = fmapL (("Sig capability parse failed: " ++) . show) $
-      compileExps (mkTextInfo _pcCode) _pcExps
-    parsed = parsePact txt
-    toPV a = fmapL (("Sig capability argument parse failed, expected simple pact value: " ++) . unpack) $ toPactValue a
+  parseJSON = withObject "SigCapability" $ \o -> SigCapability
+    <$> o .: "name"
+    <*> o .: "args"
 
 -- | Literate boolean to signal whether cap was already in scope.
 data CapAcquireResult
