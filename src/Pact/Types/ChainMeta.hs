816e603b9396da55a75087e7c7c1452b9b12141d
Stuart Popejoy <slpopejoy@users.noreply.github.com>
2019-12-18 15:15:43 -0500

    Improve ApiReq for creationTime, TTL, haddocks for PublicMeta and friends

--- src/Pact/Types/ChainMeta.hs
+++ src/Pact/Types/ChainMeta.hs
@@ -25,6 +25,7 @@ module Pact.Types.ChainMeta
   , aFrom, aTo
   , pmAddress, pmChainId, pmSender, pmGasLimit, pmGasPrice, pmTTL, pmCreationTime
   , pdPublicMeta, pdBlockHeight, pdBlockTime, pdPrevBlockHash
+  , getCurrentCreationTime
   ) where
 
 
@@ -41,6 +42,8 @@ import Data.Serialize (Serialize)
 import Data.Set (Set)
 import Data.String (IsString)
 import Data.Text
+import Data.Thyme.Clock.POSIX (getPOSIXTime)
+import Data.Thyme.Time.Core (toMicroseconds)
 import Data.Word (Word64)
 
 -- internal pact modules
@@ -50,7 +53,7 @@ import Pact.Types.ChainId (ChainId)
 import Pact.Types.Gas
 import Pact.Types.Util (AsString, lensyToJSON, lensyParseJSON)
 
-
+-- | Name of "entity", ie confidential counterparty in an encrypted exchange, in privacy-supporting platforms.
 newtype EntityName = EntityName Text
   deriving stock (Eq, Ord, Generic)
   deriving newtype (Show, NFData, Hashable, Serialize, Default, ToJSON, FromJSON, IsString, AsString)
@@ -67,6 +70,10 @@ newtype TxCreationTime = TxCreationTime ParsedInteger
   deriving stock (Eq, Ord, Generic)
   deriving newtype (Show, Num, NFData, ToJSON, FromJSON, Serialize)
 
+-- | Get current time as TxCreationTime
+getCurrentCreationTime :: IO TxCreationTime
+getCurrentCreationTime = TxCreationTime . fromIntegral . (`div` 1000000) . toMicroseconds <$> getPOSIXTime
+
 -- | Confidential/Encrypted addressing info, for use in metadata on privacy-supporting platforms.
 data Address = Address
   { _aFrom :: EntityName
@@ -79,6 +86,7 @@ instance ToJSON Address where toJSON = lensyToJSON 2
 instance FromJSON Address where parseJSON = lensyParseJSON 2
 makeLenses ''Address
 
+-- | Private-blockchain specific metadata.
 newtype PrivateMeta = PrivateMeta { _pmAddress :: Maybe Address }
   deriving (Eq,Show,Generic)
 makeLenses ''PrivateMeta
@@ -89,14 +97,21 @@ instance FromJSON PrivateMeta where parseJSON = lensyParseJSON 3
 instance NFData PrivateMeta
 instance Serialize PrivateMeta
 
--- | Contains all necessary metadata for a Chainweb-style public chain.
+-- | Allows user to specify execution parameters specific to public-chain
+-- execution, namely gas parameters, TTL, creation time, chain identifier.
 data PublicMeta = PublicMeta
   { _pmChainId :: !ChainId
+    -- ^ platform-specific chain identifier, e.g. "0"
   , _pmSender :: !Text
+    -- ^ sender gas account key
   , _pmGasLimit :: !GasLimit
+    -- ^ gas limit (maximum acceptable gas units for tx)
   , _pmGasPrice :: !GasPrice
+    -- ^ per-unit gas price
   , _pmTTL :: !TTLSeconds
+    -- ^ TTL in seconds
   , _pmCreationTime :: !TxCreationTime
+    -- ^ Creation time in seconds since UNIX epoch
   } deriving (Eq, Show, Generic)
 makeLenses ''PublicMeta
 
@@ -140,11 +155,17 @@ instance HasPlafMeta () where
   getPrivateMeta = const def
   getPublicMeta = const def
 
+-- | "Public chain" data with immutable block data
+-- height, hash, creation time
 data PublicData = PublicData
   { _pdPublicMeta :: !PublicMeta
+    -- ^ 'PublicMeta' data from request
   , _pdBlockHeight :: !Word64
+    -- ^ block height as specified by platform.
   , _pdBlockTime :: !Int64
+    -- ^ block creation time, micros since UNIX epoch
   , _pdPrevBlockHash :: !Text
+    -- ^ block hash of preceding block
   }
   deriving (Show, Eq, Generic)
 makeLenses ''PublicData
