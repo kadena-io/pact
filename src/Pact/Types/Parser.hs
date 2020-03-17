e5abd0f71c3feb42d97d9def1a8e93e8ff49bbd2
Colin Woodbury <colin@fosskers.ca>
2020-03-17 13:50:51 -0700

    `PactParser` doesn't need `MonadFail`

--- src/Pact/Types/Parser.hs
+++ src/Pact/Types/Parser.hs
@@ -22,15 +22,12 @@ module Pact.Types.Parser
 import Control.Applicative
 import Control.Monad
 import Text.Trifecta
-#if !MIN_VERSION_base(4,13,0)
-import Control.Monad.Fail (MonadFail)
-#endif
 import qualified Data.HashSet as HS
 import Text.Parser.Token.Highlight
 import Text.Parser.Token.Style
 
 newtype PactParser p a = PactParser { unPactParser :: p a }
-  deriving (Functor, Applicative, Alternative, Monad, MonadPlus, MonadFail, Parsing, CharParsing, DeltaParsing)
+  deriving (Functor, Applicative, Alternative, Monad, MonadPlus, Parsing, CharParsing, DeltaParsing)
 
 instance TokenParsing p => TokenParsing (PactParser p) where
   someSpace   = PactParser $ buildSomeSpaceParser someSpace $ CommentStyle "" "" ";" False
