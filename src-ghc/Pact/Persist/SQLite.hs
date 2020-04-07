4297c8dc4da2f039de0fdee048992d1e912f3f37
Linda Ortega <LindaOrtega@users.noreply.github.com>
2020-04-07 12:29:41 -0400

    Better sanitization of sqlite db names (#787)
    * Preliminary fix to qualified name db error
    
    * Use sql quoting to circumvent sanitizing table names
    
    * Adds qualified user table to regression test

--- src-ghc/Pact/Persist/SQLite.hs
+++ src-ghc/Pact/Persist/SQLite.hs
@@ -16,7 +16,6 @@ import Control.Monad
 import Control.Monad.Except hiding (liftEither)
 import Data.Aeson
 import qualified Data.Map.Strict as M
-import qualified Data.Text as T
 import Data.Text.Encoding
 import Data.String
 import Database.SQLite3.Direct as SQ3
@@ -58,11 +57,11 @@ toUtf8 :: Text -> Utf8
 toUtf8 = Utf8 . encodeUtf8
 
 tableName :: Table k -> Utf8
-tableName (DataTable t) = toUtf8 $ sanitize t <> "_DATA"
-tableName (TxTable t) = toUtf8 $ sanitize t <> "_TX"
+tableName (DataTable t) = toUtf8 $ "[" <> sanitize t <> "_DATA" <> "]"
+tableName (TxTable t) = toUtf8 $ "[" <> sanitize t <> "_TX" <> "]"
 
 sanitize :: TableId -> Text
-sanitize (TableId t) = T.replace "-" "_" t
+sanitize (TableId t) = t
 
 persister :: Persister SQLite
 persister = Persister {
