32ef47d58e92e87cab1f5265a58c25ffc4b3f314
Jacquin Mininger <jacquin.mininger@gmail.com>
2021-06-08 09:23:25 -0400

    cleanup nix string nonsense

--- default.nix
+++ default.nix
@@ -6,5 +6,5 @@ let
 in kpkgs.pkgs.writeScriptBin "pact" ''
   #!/usr/bin/env bash
   export PATH=${z3}/bin:$PATH
-  exec ${pactExe}/bin/pact "${"$" + "{@:2}" }"
+  exec ${pactExe}/bin/pact "''${@:2}"
 ''
