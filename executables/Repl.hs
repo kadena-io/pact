275ca4470c628f817dcff485955c032326ba2eeb
Stuart Popejoy <8353613+sirlensalot@users.noreply.github.com>
2021-06-23 11:56:31 -0400

    Restore tests and remove CPP from executables (branch on #882) (#888)
    * Restore tests and remove CPP from executables
    
    * fix tests
    
    Co-authored-by: Stuart Popejoy <slpopejoy@users.noreply.github.com>

--- executables/Repl.hs
+++ executables/Repl.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE CPP #-}
-
 -- |
 -- Copyright   :  (C) 2016 Stuart Popejoy
 -- License     :  BSD-style (see the file LICENSE)
@@ -7,14 +5,7 @@
 
 module Main where
 
-#if !defined(ghcjs_HOST_OS) && defined(BUILD_TOOL)
 import qualified Pact.Main as Repl
-#endif
 
 main :: IO ()
-main =
-#if !defined(ghcjs_HOST_OS) && defined(BUILD_TOOL)
-    Repl.main
-#else
-    error "Error: command line REPL does not exist in GHCJS mode"
-#endif
+main = Repl.main
