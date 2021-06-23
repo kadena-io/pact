4e1d2c19e4c4ef26e80afb0919481e35d4285cc2
Jacquin Mininger <jacquin.mininger@gmail.com>
2021-06-23 14:34:57 -0400

    remove redundant pact build from ci

--- project.nix
+++ project.nix
@@ -28,8 +28,8 @@ in {
             mkdir -p log  # needed or else bench can't create a sqlitedb at log/sqlite.db
             ./Setup bench
           '';
-          doCheck = false; # As this build is only for CI, we can check pact with the regular drv
-          doHaddock = false;
+          doHaddock = true;
+          doCheck = true;
         });
       };
       packages = {
