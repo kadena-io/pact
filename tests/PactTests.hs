4231cec15b980d65700be3f413494366cdb89886
Stuart Popejoy <slpopejoy@users.noreply.github.com>
2021-06-23 19:48:46 -0400

    Tests with and without build-tool; no more hspec-discover

---
+++ tests/PactTests.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE CPP #-}
+
+import Test.Hspec
+
+import qualified Blake2Spec
+import qualified KeysetSpec
+import qualified TypesSpec
+
+#ifndef ghcjs_HOST_OS
+import qualified PactTestsSpec
+import qualified ParserSpec
+import qualified SchemeSpec
+import qualified SignatureSpec
+
+# ifdef BUILD_TOOL
+import qualified AnalyzePropertiesSpec
+import qualified AnalyzeSpec
+import qualified ClientSpec
+import qualified DocgenSpec
+import qualified GasModelSpec
+import qualified GoldenSpec
+import qualified HistoryServiceSpec
+import qualified PactContinuationSpec
+import qualified PersistSpec
+import qualified RemoteVerifySpec
+import qualified TypecheckSpec
+
+# endif
+#endif
+
+main :: IO ()
+main = hspec $ do
+
+  describe "Blake2Spec" Blake2Spec.spec
+  describe "KeysetSpec" KeysetSpec.spec
+  describe "TypesSpec" TypesSpec.spec
+
+#ifndef ghcjs_HOST_OS
+
+  describe "PactTestsSpec" PactTestsSpec.spec
+  describe "ParserSpec" ParserSpec.spec
+  describe "SignatureSpec" SignatureSpec.spec
+  describe "SchemeSpec" SchemeSpec.spec
+
+# ifdef BUILD_TOOL
+
+  describe "AnalyzePropertiesSpec" AnalyzePropertiesSpec.spec
+  describe "AnalyzeSpec" AnalyzeSpec.spec
+  describe "ClientSpec" ClientSpec.spec
+  describe "DocgenSpec" DocgenSpec.spec
+  describe "GasModelSpec" GasModelSpec.spec
+  describe "GoldenSpec" GoldenSpec.spec
+  describe "HistoryServiceSpec" HistoryServiceSpec.spec
+  describe "PactContinuationSpec" PactContinuationSpec.spec
+  describe "PersistSpec" PersistSpec.spec
+  describe "RemoteVerifySpec" RemoteVerifySpec.spec
+  describe "TypecheckSpec" TypecheckSpec.spec
+
+# endif
+#endif
