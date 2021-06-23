47048542edf5fbd620ac38d3b698d0b52053da68
Stuart Popejoy <slpopejoy@users.noreply.github.com>
2021-06-23 01:20:27 -0400

    Restore tests and remove CPP from executables

--- executables/Bench.hs
+++ executables/Bench.hs
@@ -1,16 +1,6 @@
-{-# LANGUAGE CPP #-}
-
 module Main where
 
-#if !defined(ghcjs_HOST_OS) && defined(BUILD_TOOL)
 import qualified Pact.Bench as Bench
-#endif
-
 
 main :: IO ()
-main =
-#if !defined(ghcjs_HOST_OS) && defined(BUILD_TOOL)
-    Bench.main
-#else
-    error "Error: command line REPL does not exist in GHCJS mode"
-#endif
+main = Bench.main
--- executables/GasModel.hs
+++ executables/GasModel.hs
@@ -1,16 +1,6 @@
-{-# LANGUAGE CPP #-}
-
 module Main where
 
-#if !defined(ghcjs_HOST_OS) && defined(BUILD_TOOL)
 import qualified Pact.GasModel.GasModel as GasModel
-#endif
-
 
 main :: IO ()
-main =
-#if !defined(ghcjs_HOST_OS) && defined(BUILD_TOOL)
-    GasModel.main
-#else
-    error "Error: gas model benchmarking does not exist in GHCJS mode"
-#endif
+main = GasModel.main
