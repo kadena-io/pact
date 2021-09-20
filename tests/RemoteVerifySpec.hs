4202e5c86250384b36b379a448e24e3f10b0e0f0
Stuart Popejoy <slpopejoy@users.noreply.github.com>
2021-09-19 21:03:42 -0400

    Fix test

--- tests/RemoteVerifySpec.hs
+++ tests/RemoteVerifySpec.hs
@@ -97,7 +97,7 @@ testSingleModule = do
 
   it "verifies over the network" $
     fmap (view Remote.responseLines) resp `shouldBe`
-    (Right [""])
+    (Right [])
 
 testUnsortedModules :: Spec
 testUnsortedModules = do
@@ -114,7 +114,7 @@ testUnsortedModules = do
 
   it "verifies over the network" $
     fmap (view Remote.responseLines) resp `shouldBe`
-    (Right [""])
+    (Right [])
   where
     code = [text|
       (env-keys ["admin"])
