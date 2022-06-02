45ba50303c13dda24690bc37f4f86a80e75bc434
Lars Kuhtz <lars@kadena.io>
2022-06-02 14:30:26 -0700

    update some outdated packages (#994)
    * update some outdated packages
    
    * fix dot visualization of analyze model
    
    * unordered-containers and sbv
    
    * Jailbreak libBF for older hashable
    
    Co-authored-by: Doug Beardsley <mightybyte@gmail.com>

--- project.nix
+++ project.nix
@@ -28,6 +28,25 @@ in {
           sha256 = "1cfn74j6dr4279bil9k0n1wff074sdlz6g1haqyyy38wm5mdd7mr";
         } {});
 
+        # sbv requires this even though it is not used in the build (and the hash is invalid)
+        tasty-bench = dontCheck (self.callHackageDirect {
+          pkg = "tasty-bench";
+          ver = "0.3.1";
+          sha256 = "0000000000000000000000000000000000000000000000000000";
+        } {});
+
+        libBF = doJailbreak (dontCheck (self.callHackageDirect {
+          pkg = "libBF";
+          ver = "0.6.3";
+          sha256 = "0j0i39jb389rnrkkw2xqz10471afxys79nf31hhlqr4fk6ddhjf7";
+        } {}));
+
+        sbv = dontCheck (self.callHackageDirect {
+          pkg = "sbv";
+          ver = "9.0";
+          sha256 = "14g2qax1vc7q4g78fa562dviqvcd0l52kd5jmgv90g3g3ci15bnl";
+        } {});
+
         pact-do-benchmark = overrideCabal (doBenchmark (dontCheck super.pact)) (oldDrv: {
           benchmarkSystemDepends = [ pkgs.z3 ];
           postBuild = ''
