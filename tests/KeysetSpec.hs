ea4d4cf479ff435bb67711217a31e88afef6acdc
Emily Pillmore <emilypi@cohomolo.gy>
2022-07-06 15:57:43 -0600

    update hspec part 1

--- tests/KeysetSpec.hs
+++ tests/KeysetSpec.hs
@@ -14,9 +14,16 @@ spec = describe "fromJSON" testFromJSON
 testFromJSON :: Spec
 testFromJSON = do
   let ks = mkKeySet ["a","b"] "keys-all" Nothing
-  it "full read from JSON" $
+      ksn = mkKeySet ["a","b"] "keys-all" (Just "ns")
+  it "full read from JSON, no namespace" $
     eitherDecode "{ \"keys\": [\"a\",\"b\"], \"pred\": \"keys-all\" }" `shouldBe` Right ks
-  it "object no pred" $
+  it "object no pred, no namespace" $
     eitherDecode "{ \"keys\": [\"a\",\"b\"]}" `shouldBe` Right ks
-  it "just list" $
+  it "just list, no namespace" $
     eitherDecode "[\"a\",\"b\"]" `shouldBe` Right ks
+  it "full read from JSON, namespaced" $
+    eitherDecode "{ \"keys\": [\"a\",\"b\"], \"pred\": \"keys-all\" }" `shouldBe` Right ksn
+  it "object no pred, namespaced" $
+    eitherDecode "{ \"keys\": [\"a\",\"b\"]}" `shouldBe` Right ksn
+  it "just list, namespaced" $
+    eitherDecode "[\"a\",\"b\"]" `shouldBe` Right ksn
