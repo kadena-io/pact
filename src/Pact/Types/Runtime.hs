59467a56f87ee6e52a3b8d54549d0fa95170dcc3
Jose C <cardona.josem@gmail.com>
2022-08-26 14:32:10 -0400

    flag legacy pow (#1041)

--- src/Pact/Types/Runtime.hs
+++ src/Pact/Types/Runtime.hs
@@ -151,6 +151,8 @@ data ExecutionFlag
   | FlagDisablePact431
   -- | Disable Pact 4.4 features
   | FlagDisablePact44
+  -- | Disable new pow impl
+  | FlagDisableNewPow
   deriving (Eq,Ord,Show,Enum,Bounded)
 
 -- | Flag string representation
