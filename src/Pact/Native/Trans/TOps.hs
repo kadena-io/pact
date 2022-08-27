243098f440a00f7b66aad895a3c3c02bc2eab9fe
John Wiegley <johnw@newartisans.com>
2022-08-26 21:04:36 -0700

    Update src/Pact/Native/Trans/TOps.hs
    Co-authored-by: Stuart Popejoy <8353613+sirlensalot@users.noreply.github.com>

--- src/Pact/Native/Trans/TOps.hs
+++ src/Pact/Native/Trans/TOps.hs
@@ -2,7 +2,7 @@
 -- Module      :  Pact.Native.Trans.TOps
 -- Copyright   :  (C) 2022 John Wiegley
 -- License     :  BSD-style (see the file LICENSE)
--- Maintainer  :  Stuart Popejoy <stuart@kadena.io>
+-- Maintainer  :  John Wiegley <john@kadena.io>
 --
 -- Operators and math built-ins.
 --
