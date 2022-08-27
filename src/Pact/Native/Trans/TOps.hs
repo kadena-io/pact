81e92c3897190369d78fdc62f5c07fc5e2cb8856
John Wiegley <johnw@newartisans.com>
2022-08-26 21:07:32 -0700

    Remove an unneeded file

--- src/Pact/Native/Trans/TOps.hs
+++ src/Pact/Native/Trans/TOps.hs
@@ -8,7 +8,36 @@
 --
 
 module Pact.Native.Trans.TOps
-    ( module X
-    ) where
+  ( trans_pow
+  , trans_exp
+  , trans_log
+  , trans_ln
+  , trans_sqrt
+  ) where
 
-import Pact.Native.Trans.Types as X
+foreign import ccall unsafe "musl_pow"
+  musl_pow :: Double -> Double -> Double
+
+foreign import ccall unsafe "musl_exp"
+  musl_exp :: Double -> Double
+
+foreign import ccall unsafe "musl_log"
+  musl_log :: Double -> Double
+
+foreign import ccall unsafe "musl_sqrt"
+  musl_sqrt :: Double -> Double
+
+trans_pow :: Double -> Double -> Double
+trans_pow = musl_pow
+
+trans_exp :: Double -> Double
+trans_exp = musl_exp
+
+trans_log :: Double -> Double -> Double
+trans_log b x = musl_log x / musl_log b
+
+trans_ln :: Double -> Double
+trans_ln = musl_log
+
+trans_sqrt :: Double -> Double
+trans_sqrt = musl_sqrt
