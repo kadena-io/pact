ce74e1f6c501c41848d63bd99a540d7e59760aab
John Wiegley <johnw@newartisans.com>
2022-11-30 17:24:51 -0800

    Add old Nix files back, they don't conflict

---
+++ default.nix
@@ -0,0 +1,10 @@
+let 
+  pactProj = import ./project.nix {};
+  inherit (pactProj) kpkgs proj;
+  z3 = proj.passthru.z3;
+  pactExe = proj.ghc.pact;
+in kpkgs.pkgs.writeScriptBin "pact" ''
+  #!/usr/bin/env bash
+  export PATH=${z3}/bin:$PATH
+  exec ${pactExe}/bin/pact "$@"
+''
---
+++ shell.nix
@@ -0,0 +1,1 @@
+(import ./project.nix {}).proj.shells.ghc
