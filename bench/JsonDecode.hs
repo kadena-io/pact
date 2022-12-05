51fa9a01644790d87bc56514fab76bdd1fa0a15b
Lars Kuhtz <lars@kadena.io>
2022-12-04 21:03:34 -0800

    fix json-decode bechmark

--- bench/JsonDecode.hs
+++ bench/JsonDecode.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
@@ -23,7 +24,7 @@ import Control.DeepSeq
 import Control.Exception
 import Control.Monad
 
-import Data.Aeson
+import Data.Aeson hiding (Result)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
@@ -36,36 +37,61 @@ import GHC.Stack
 
 import System.Clock
 
-import Test.QuickCheck
+import Test.QuickCheck hiding (Result)
 
 -- internal modules
 
 import Pact.Types.Persistence
-import Pact.Types.Term
+import Pact.Types.Term hiding (Object)
 import Pact.Types.Term.Arbitrary ()
 
-resultsToCsv :: [(T.Text, TimeSpec, Int)] -> TL.Text
-resultsToCsv x = TB.toLazyText $ "size,time,source\n" <> go x
+-- -------------------------------------------------------------------------- --
+-- Tools
+
+resultsToCsv :: [(T.Text, Result)] -> TL.Text
+resultsToCsv x = TB.toLazyText $ "source,time,size,maxDepth,minDepth,valueSize\n" <> go x
   where
     go [] = mempty
-    go ((l, a, b):t)
-        = TB.decimal b
+    go ((l,(a0,a1,a2,a3,a4)):t)
+        = TB.fromText l
         <> TB.singleton ','
-        <> TB.decimal (toNanoSecs a)
+        <> TB.decimal (toNanoSecs a0)
         <> TB.singleton ','
-        <> TB.fromText l
+        <> TB.decimal a1
+        <> TB.singleton ','
+        <> TB.decimal a2
+        <> TB.singleton ','
+        <> TB.decimal a3
+        <> TB.singleton ','
+        <> TB.decimal a4
         <> TB.singleton '\n'
         <> go t
 
 -- -------------------------------------------------------------------------- --
+-- Json Tools
+
+valueMetric :: (forall f . Foldable f => f Int -> Int) -> Value -> Int
+valueMetric f (Object a)
+    | mempty == a = 1
+    | otherwise = 1 + f (valueMetric f <$> a)
+valueMetric f (Array a)
+    | mempty == a = 1
+    | otherwise = 1 + f (valueMetric f <$> a)
+valueMetric _ _ = 1
+
+valueMaxDepth :: Value -> Int
+valueMaxDepth = valueMetric maximum
+
+valueMinDepth :: Value -> Int
+valueMinDepth = valueMetric minimum
+
+valueSize :: Value -> Int
+valueSize = valueMetric sum
+
+-- -------------------------------------------------------------------------- --
 -- benchmarks
 
-stopWatch :: NFData a => IO a -> IO (TimeSpec, a)
-stopWatch a = do
-    s <- getTime Monotonic
-    !x <- a >>= \x -> return $!! x
-    e <- getTime Monotonic
-    return (diffTimeSpec e s, x)
+type Result = (TimeSpec, Int, Int, Int, Int)
 
 benchDecode
     :: forall a
@@ -73,12 +99,22 @@ benchDecode
     => FromJSON a
     => NFData a
     => B.ByteString
-    -> IO (TimeSpec, Int)
+    -> IO Result
 benchDecode bytes = do
-    (t, x) <- stopWatch $! evaluate $! eitherDecodeStrict' @a bytes
-    case x of
+    s <- getTime Monotonic
+    !r <- evaluate $! eitherDecodeStrict' @a bytes
+    e <- getTime Monotonic
+    case r of
         Left err -> error $ "failed: " <> err
-        Right !_ -> return (t, B.length bytes)
+        Right !_ -> do
+            let Right v = eitherDecodeStrict' @Value bytes
+            return
+                ( diffTimeSpec e s
+                , B.length bytes
+                , valueMaxDepth v
+                , valueMinDepth v
+                , valueSize v
+                )
 
 benchDecodeArbitrary
     :: forall a
@@ -88,7 +124,7 @@ benchDecodeArbitrary
     => NFData a
     => FromJSON a
     => Int
-    -> IO (TimeSpec, Int)
+    -> IO Result
 benchDecodeArbitrary = benchDecodeArbitrary_ @a @a
 
 benchDecodeArbitrary_
@@ -99,7 +135,7 @@ benchDecodeArbitrary_
     => FromJSON b
     => NFData b
     => Int
-    -> IO (TimeSpec, Int)
+    -> IO Result
 benchDecodeArbitrary_ i = do
     !t <- generate @a $ scale (\_ -> i) arbitrary
     benchDecode @b (BL.toStrict $ encode $! t)
@@ -110,13 +146,13 @@ benchDecodeFile
     => FromJSON a
     => NFData a
     => FilePath
-    -> IO (TimeSpec, Int)
+    -> IO Result
 benchDecodeFile file = do
     !bytes <- B.readFile file
     benchDecode @a $! bytes
 
-tag :: T.Text -> (TimeSpec, a) -> (T.Text, TimeSpec, a)
-tag l (a,b) = (l,a,b)
+tag :: T.Text -> Result -> (T.Text, Result)
+tag l r = (l, r)
 
 -- -------------------------------------------------------------------------- --
 -- main 2
@@ -132,8 +168,8 @@ main = do
 
     -- benchmark arbitrary
     print "========= arbitrary:"
-    l1 <- flip foldMap [0..1000 :: Int] $ \i -> do
-        when (mod i (50::Int) == 0) $ print i
+    l1 <- flip foldMap [0,100..100000 :: Int] $ \i -> do
+        when (mod i (1000::Int) == 0) $ print i
         !val <- generate $ scale (const i) $ arbitrary @(Term (Ref' PersistDirect))
         !bytes <- evaluate $! BL.toStrict $ encode $! val
 
