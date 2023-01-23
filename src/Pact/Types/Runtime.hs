a72535c72f6ae79a1a90a004bfb70396148e9cb0
Greg Hale <imalsogreg@gmail.com>
2023-01-22 20:59:52 -0800

    add session pubkey to EvalEnv

--- src/Pact/Types/Runtime.hs
+++ src/Pact/Types/Runtime.hs
@@ -194,6 +194,8 @@ data EvalEnv e = EvalEnv {
       _eeRefStore :: !RefStore
       -- | Verified keys from message.
     , _eeMsgSigs :: !(M.Map PublicKeyText (S.Set UserCapability))
+      -- | Keys of the user who has an active Webauthn session.
+    , _eeSessionSig :: !(Maybe (PublicKeyText, S.Set UserCapability))
       -- | JSON body accompanying message.
     , _eeMsgBody :: !Value
       -- | Execution mode
