39d82c0ff7b3097b40b2a8ffb2916f82b15e73e9
Stuart Popejoy <sirlensalot@users.noreply.github.com>
2023-02-25 13:02:40 -0500

    add golden

--- tests/CoverageSpec.hs
+++ tests/CoverageSpec.hs
@@ -4,13 +4,11 @@ module CoverageSpec (spec) where
 import Control.Lens
 import Control.Monad
 import Test.Hspec
-import Data.Default
 
 import Pact.Coverage
 import Pact.Repl
 import Pact.Repl.Types
 import Pact.Types.Runtime
-
 
 spec :: Spec
 spec = do
@@ -18,9 +16,13 @@ spec = do
 
 
 testCover :: Spec
-testCover = runIO $ do
-  let fn = "tests/lcov/lcov.repl"
-  (ref,adv) <- mkCoverageAdvice
-  s <- set (rEnv . eeAdvice) adv <$> initReplState (Script False fn) Nothing
-  void $! execScriptState' fn s (set (rEnv . eeAdvice) def)
-  writeCovReport ref
+testCover = do
+  runIO $ do
+    let fn = "tests/lcov/lcov.repl"
+    (ref,adv) <- mkCoverageAdvice
+    s <- initReplState (Script False fn) Nothing
+    void $! execScriptState' fn s (set (rEnv . eeAdvice) adv)
+    writeCovReportInDir "tests/lcov" ref
+  golden <- runIO $ readFile "tests/lcov/coverage/lcov.info.golden"
+  actual <- runIO $ readFile "tests/lcov/coverage/lcov.info"
+  it "should match golden" $ actual `shouldBe` golden
