7c7d2bab15e9295ae25e2c448588213d748c4f7d
Stuart Popejoy <sirlensalot@users.noreply.github.com>
2023-02-25 19:20:37 -0500

    actual goldens

--- tests/CoverageSpec.hs
+++ tests/CoverageSpec.hs
@@ -3,9 +3,14 @@ module CoverageSpec (spec) where
 
 import Control.Lens
 import Control.Monad
+import Data.IORef
+import qualified Data.Text as T
+
 import Test.Hspec
+import Test.Hspec.Golden
 
 import Pact.Coverage
+import Pact.Coverage.Report
 import Pact.Repl
 import Pact.Repl.Types
 import Pact.Types.Runtime
@@ -17,12 +22,18 @@ spec = do
 
 testCover :: Spec
 testCover = do
-  runIO $ do
+  rpt <- runIO $ do
     let fn = "tests/lcov/lcov.repl"
     (ref,adv) <- mkCoverageAdvice
     s <- initReplState (Script False fn) Nothing
     void $! execScriptState' fn s (set (rEnv . eeAdvice) adv)
-    writeCovReportInDir "tests/lcov" ref
-  golden <- runIO $ readFile "tests/lcov/coverage/lcov.info.golden"
-  actual <- runIO $ readFile "tests/lcov/coverage/lcov.info"
-  it "should match golden" $ actual `shouldBe` golden
+    readIORef ref
+  it "matches golden coverage" $ Golden
+      { output = T.unpack (showReport rpt)
+      , encodePretty = id
+      , writeToFile = writeFile
+      , readFromFile = readFile
+      , testName = "lcov"
+      , directory = "golden"
+      , failFirstTime = False
+      }
