1323a786a5cceb9566abfde426a9951620ccc286
Robert Soeldner <r.soeldner@gmail.com>
2023-04-04 21:11:10 +0200

    use pseudo-terminal for unit test of repl output

--- tests/ReplSpec.hs
+++ tests/ReplSpec.hs
@@ -7,56 +7,40 @@ import Test.Hspec
 
 import Data.ByteString (ByteString)
 import Control.Monad.IO.Class
-import System.Process
 import qualified Data.ByteString as BS
 import Control.Concurrent (threadDelay)
-import GHC.IO.Handle (hClose)
+import System.Posix.Pty
+import Control.Monad (void)
 
 spec :: Spec
 spec = describe "ReplSpec" $ do
   it "should not print literal via `Show` (regression #1101)" $ do
     let src = "(print 1)"
     out <- liftIO (runInteractive src)
-    out `shouldSatisfy` outputSatisfy containsNoTermInfo
+    out `shouldSatisfy` containsNoTermInfo
 
   it "should not print `ErrInfo` via `Show` (regression #1164)" $ do
     let src = "(module m g (defcap g () true) (defcap OFFERED (pid:string) true) \
               \ (defpact sale () (step (= (create-capability-guard (OFFERED (pact-id)))\
               \ (create-capability-guard (OFFERED pact-id)))))))"
     out <- liftIO (runInteractive src)
-    out `shouldSatisfy` outputSatisfy containsNoErrInfo
+    out `shouldSatisfy`containsNoErrInfo
 
 
-runInteractive :: ByteString -> IO (ByteString, ByteString)
+runInteractive :: ByteString -> IO ByteString
 runInteractive src = do
-  let cp = (shell "pact")
-        { std_in  = CreatePipe
-        , std_out = CreatePipe
-        , std_err = CreatePipe
-        }
-  (Just inH, Just outH, Just errH, ph) <- createProcess cp
-  BS.hPut inH src
-  hClose inH
-  threadDelay 20000
-  (out, err) <- readTillPrompt outH errH mempty mempty
-
-  terminateProcess ph
-
-  pure (out, err)
+  (pty, _) <- spawnWithPty Nothing True "pact" [] (100,100)
+  threadDelay 2000
+  writePty pty (src <> "\n")
+  threadDelay 2000
+  void $ go pty mempty -- the first pact prompt
+  go pty mempty
   where
-    readTillPrompt outH errH o e = do
-      out <- BS.hGetNonBlocking outH 4046
-      err <- BS.hGetNonBlocking errH 4096
-
-      pure (o <> out, e <> err)
-
-      -- if not ("pact>" `BS.isInfixOf` err)
-      --   then threadDelay 20000 >>  readTillPrompt outH errH o e
-      --   else pure (o <> out, e <> err)
-
-
-outputSatisfy :: (ByteString -> Bool) -> (ByteString, ByteString) ->Bool
-outputSatisfy f (o, e) = f o && f e
+    go pty o = do
+      content <- readPty pty
+      if "pact>" `BS.isInfixOf` content
+      then pure o
+      else go pty (o <> content)
 
 containsNoErrInfo :: ByteString -> Bool
 containsNoErrInfo s = not ("_errDeltas" `BS.isInfixOf` s)
