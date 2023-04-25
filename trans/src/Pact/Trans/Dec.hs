ad8f198ee4579000514c59610600242755d25bfb
John Wiegley <johnw@newartisans.com>
2023-04-24 23:13:38 -0700

    The tests are now passing

--- trans/src/Pact/Trans/Dec.hs
+++ trans/src/Pact/Trans/Dec.hs
@@ -4,6 +4,8 @@
 {-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE DeriveTraversable #-}
 
+{-# OPTIONS_GHC -Wno-incomplete-record-updates #-}
+
 -- |
 -- Module      :  Pact.Trans.Dec
 -- Copyright   :  (C) 2023 John Wiegley
@@ -31,15 +33,40 @@ import Pact.Trans.Types
 
 type Prec = P100
 
+{-
+decShow :: D.Decimal -> String
+decShow D.Decimal { D.decimalPlaces = e, D.decimalMantissa = c } =
+  "Dec { places = "
+    ++ show e ++ ", mantissa = "
+    ++ show c ++ " }"
+
+numShow :: Decimal p r -> String
+numShow N.Num { N.sign = s, N.coefficient = c, N.exponent = e } =
+  "Num { sign = "
+    ++ (case s of Op.Neg -> "Neg"; Op.Pos -> "Pos") ++ ", coefficient = "
+    ++ show c ++ ", exponent = "
+    ++ show e ++ " }"
+numShow _ = error "unexpected"
+-}
+
 toDecimal :: Decimal p r -> TransResult D.Decimal
-toDecimal N.Num { N.sign = s, N.coefficient = c, N.exponent = e } =
-  TransNumber $
-    D.Decimal { D.decimalPlaces = negate (fromIntegral e)
-              , D.decimalMantissa =
-                (case s of
-                   Op.Neg -> negate
-                   Op.Pos -> id) (fromIntegral c)
-              }
+toDecimal N.Num { N.sign = s, N.coefficient = c, N.exponent = e }
+  | e < 0 =
+    TransNumber $
+      D.Decimal { D.decimalPlaces = negate (fromIntegral e)
+                , D.decimalMantissa =
+                  (case s of
+                     Op.Neg -> negate
+                     Op.Pos -> id) (fromIntegral c)
+                }
+  | otherwise =
+    TransNumber $
+      D.Decimal { D.decimalPlaces = 0
+                , D.decimalMantissa =
+                  (case s of
+                     Op.Neg -> negate
+                     Op.Pos -> id) (fromIntegral c) * (10^e)
+                }
 toDecimal (N.Inf Op.Neg) = TransNegInf 0
 toDecimal (N.Inf Op.Pos) = TransInf 0
 toDecimal N.NaN {} = TransNaN 0
@@ -49,37 +76,43 @@ fromDecimal D.Decimal { D.decimalPlaces = e, D.decimalMantissa = c } =
   N.Num { N.sign = if c < 0
                    then Op.Neg
                    else Op.Pos
-        , N.coefficient = fromIntegral c
+        , N.coefficient = fromIntegral (abs c)
         , N.exponent = negate (toInteger e)
         }
 
+reduce_ :: (Precision p, Rounding r) => Decimal p r -> Decimal p r
+reduce_ n@N.Num { N.coefficient = c, N.exponent = e }
+  | c == 0 =         n {                    N.exponent = 0     }
+  | r == 0 = reduce_ n { N.coefficient = q, N.exponent = e + 1 }
+  where
+    (q, r) = c `quotRem` 10
+reduce_ n = n
+
 dec_reduce :: D.Decimal -> D.Decimal
 dec_reduce x =
-  case evalArith (Op.reduce (fromDecimal x :: ExtendedDecimal Prec)
-                  :: Arith Prec RoundHalfEven (ExtendedDecimal Prec))
-       extendedDefaultContext of
-    Left err -> error $ "reduce error: " ++ show err
-    Right n -> case toDecimal $ n of
-      TransNumber n' -> n'
-      e -> error $ "reduce error: " ++ show e
+  case toDecimal (reduce_ (fromDecimal x :: ExtendedDecimal Prec)) of
+    TransNumber n -> n
+    e -> error $ "reduce error: " ++ show e
 
 dec_exp :: D.Decimal -> TransResult D.Decimal
-dec_exp x = toDecimal $
-  case evalArith (Op.reduce =<< Op.exp (fromDecimal x :: ExtendedDecimal Prec)
+dec_exp x =
+  case evalArith (Op.exp (fromDecimal x :: ExtendedDecimal Prec)
                   :: Arith Prec RoundHalfEven (ExtendedDecimal Prec))
        extendedDefaultContext of
     Left err -> error $ "exp error: " ++ show err
-    Right n -> n
+    Right n -> toDecimal n
 
 dec_ln :: D.Decimal -> TransResult D.Decimal
-dec_ln x = toDecimal $
-  case evalArith (Op.reduce =<< Op.ln (fromDecimal x :: ExtendedDecimal Prec)
+dec_ln x =
+  case evalArith (Op.ln (fromDecimal x :: ExtendedDecimal Prec)
                   :: Arith Prec RoundHalfEven (ExtendedDecimal Prec))
        extendedDefaultContext of
     Left err -> error $ "ln error: " ++ show err
-    Right n -> n
+    Right n -> toDecimal n
 
 dec_log :: D.Decimal -> D.Decimal -> TransResult D.Decimal
+dec_log D.Decimal{ D.decimalPlaces = _, D.decimalMantissa = 0 } _ =
+  TransNumber 0.0
 dec_log b x =
   case dec_ln x of
     TransNumber x' ->
@@ -89,18 +122,21 @@ dec_log b x =
     x' -> x'
 
 dec_pow :: D.Decimal -> D.Decimal -> TransResult D.Decimal
-dec_pow x y = toDecimal $
-  case evalArith (Op.reduce =<< Op.power (fromDecimal x :: ExtendedDecimal Prec)
+dec_pow D.Decimal{ D.decimalPlaces = _, D.decimalMantissa = 0 }
+        D.Decimal{ D.decimalPlaces = _, D.decimalMantissa = 0 } =
+  TransNumber 1.0
+dec_pow x y =
+  case evalArith (Op.power (fromDecimal x :: ExtendedDecimal Prec)
                    (fromDecimal y :: ExtendedDecimal Prec)
                   :: Arith Prec RoundHalfEven (ExtendedDecimal Prec))
        extendedDefaultContext of
     Left err -> error $ "pow error: " ++ show err
-    Right n -> n
+    Right n -> toDecimal n
 
 dec_sqrt :: D.Decimal -> TransResult D.Decimal
-dec_sqrt x = toDecimal $
-  case evalArith (Op.reduce =<< Op.squareRoot (fromDecimal x :: ExtendedDecimal Prec)
+dec_sqrt x =
+  case evalArith (Op.squareRoot (fromDecimal x :: ExtendedDecimal Prec)
                   :: Arith Prec RoundHalfEven (ExtendedDecimal Prec))
        extendedDefaultContext of
     Left err -> error $ "sqrt error: " ++ show err
-    Right n -> n
+    Right n -> toDecimal n
