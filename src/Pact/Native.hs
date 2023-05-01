49eb58463210dc4cfcd10fdff651a08dad9ff932
Emily Pillmore <emily@kadena.io>
2023-05-01 14:28:43 -0600

    revert doc changes

--- src/Pact/Native.hs
+++ src/Pact/Native.hs
@@ -544,14 +544,12 @@ defineNamespaceDef = setTopLevelOnly $ defGasRNative "define-namespace" defineNa
 namespaceDef :: NativeDef
 namespaceDef = setTopLevelOnly $ defGasRNative "namespace" namespace
   (funType tTyString [("namespace", tTyString)])
-  [LitExample "(namespace 'my-namespace)"
-  ,LitExample "(namespace \"\")"]
+  [LitExample "(namespace 'my-namespace)"]
   "Set the current namespace to NAMESPACE. All expressions that occur in a current \
   \transaction will be contained in NAMESPACE, and once committed, may be accessed \
   \via their fully qualified name, which will include the namespace. Subsequent \
   \namespace calls in the same tx will set a new namespace for all declarations \
-  \until either the next namespace declaration, or the end of the tx. If NAMESPACE \
-  \is the empty string, env namespace will be reset to the root space."
+  \until either the next namespace declaration, or the end of the tx."
   where
     namespace :: GasRNativeFun e
     namespace g i as = case as of
