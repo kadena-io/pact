88d161d32f3e1b199296bd2fed8eca8813db45d5
rsoeldner <r.soeldner@gmail.com>
2023-08-29 10:50:34 +0200

    add diff-time to FV

--- tests/AnalyzeSpec.hs
+++ tests/AnalyzeSpec.hs
@@ -2333,6 +2333,24 @@ spec = describe "analyze" $ do
               ))
           |]
     in expectPass code $ Valid $ sNot Abort'
+
+  describe "diff-time implementation (#1291)" $
+    let code =
+          [text|
+            (defun test:decimal ()
+              @model[(property (= result -60.0))]
+              (diff-time (time "2021-01-01T00:00:00Z") (time "2021-01-01T00:01:00Z")))
+
+            (defun test1:decimal ()
+              @model[(property (= result -61.0))]
+              (diff-time (time "2021-01-01T00:00:00Z") (time "2021-01-01T00:01:01Z")))
+
+            (defun test2:decimal ()
+              @model[(property (= result 1.0))]
+              (diff-time (time "2021-01-01T00:00:01Z") (time "2021-01-01T00:00:00Z")))
+          |]
+    in expectVerified code
+
   describe "regression time representation (int64/integer)" $
     let code =
           [text|
