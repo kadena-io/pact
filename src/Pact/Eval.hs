e0f3bd07d4cf6fa5b4742e2046e8c2ec1d2c87db
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2023-12-07 14:16:36 +0100

    Replace liftA2 with <$> and <*>

--- src/Pact/Eval.hs
+++ src/Pact/Eval.hs
@@ -53,7 +53,6 @@ module Pact.Eval
     ) where
 
 import Bound
-import qualified Control.Applicative
 import Control.Lens hiding (DefName)
 import Control.DeepSeq
 import Control.Monad
@@ -756,7 +755,7 @@ fullyQualifyDefs info mdef defs = do
           && mn == _mnName (_mName mdef)
           && isNsMatch -> resolveBareName memo (BareName fn i)
           where
-            isNsMatch = fromMaybe True (Control.Applicative.liftA2 (==) modNs mNs)
+            isNsMatch = fromMaybe True ((==) <$> modNs <*> mNs)
             modNs = _mnNamespace (_mName mdef)
       f  -> do
         dm <- lift (resolveRefFQN f f) -- lookup ref, don't try modules for barenames
