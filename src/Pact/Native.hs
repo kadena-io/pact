01253f00521974ace199642d6d65638b5978c802
chessai <chessai1996@gmail.com>
2024-02-13 12:16:04 -0600

    add example to hyperlane-message-id Pact Native

--- src/Pact/Native.hs
+++ src/Pact/Native.hs
@@ -1583,7 +1583,9 @@ hyperlaneMessageIdDef = defGasRNative
   "hyperlane-message-id"
   hyperlaneMessageId'
   (funType tTyString [("x", tTyObjectAny)])
-  []
+  [
+    "(hyperlane-message-id {\"destinationDomain\": 1,\"nonce\": 325,\"originDomain\": 626,\"recipient\": \"0x71C7656EC7ab88b098defB751B7401B5f6d8976F\",\"sender\": \"0x6b622d746f6b656e2d726f75746572\",\"tokenMessage\": {\"amount\": 10000000000000000000.0,\"recipient\": \"0x71C7656EC7ab88b098defB751B7401B5f6d8976F\"},\"version\": 1})"
+  ]
   "Get the Message Id of a Hyperlane Message object."
   where
     hyperlaneMessageId' :: RNativeFun e
