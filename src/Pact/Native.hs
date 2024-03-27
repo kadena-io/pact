4cfe7d14d77f8a00d23c5eb5629198a47e640463
jmcardon <cardona.josem@gmail.com>
2024-03-27 12:39:49 -0400

    remove stale comment

--- src/Pact/Native.hs
+++ src/Pact/Native.hs
@@ -1625,8 +1625,6 @@ hyperlaneMessageIdDef ty = defGasRNative
   "hyperlane-message-id"
   hyperlaneMessageId'
   (funType tTyString [("x", tTyObject ty)])
-  -- (funType tTyString [("x", tTyObject (TyUser (snd hyperlaneDataSchema)))])
-  -- (funType tTyString [("x", tTyObjectAny)])
   [
     "(hyperlane-message-id {\"destinationDomain\": 1,\"nonce\": 325,\"originDomain\": 626,\"recipient\": \"0x71C7656EC7ab88b098defB751B7401B5f6d8976F\",\"sender\": \"0x6b622d746f6b656e2d726f75746572\",\"tokenMessage\": {\"amount\": 10000000000000000000.0,\"recipient\": \"0x71C7656EC7ab88b098defB751B7401B5f6d8976F\"},\"version\": 1})"
   ]
